4c12d9e6b3d6f145cd0d730f00051b707b03ab86
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-09-10 17:23:41 +0100

    Creation of Navbar and SearchBar component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Navbar } from "./Navbar";
 
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchFeed } from "./SearchFeed";
+export { default as SearchBar } from "./SearchBar";
